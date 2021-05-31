105533efdce953ad694e536a252f39b1fa926aa9
jukka <jukka4996@gmail.com>
2021-05-31 15:28:07 -0300

    add utrances

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Prismic from '@prismicio/client';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
@@ -10,9 +11,12 @@ import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 import { getPrismicClient } from '../../services/prismic';
 import { readTime } from '../../utils/readTime';
 import styles from './post.module.scss';
+import { useEffect } from 'react';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: { url: string };
@@ -33,6 +37,18 @@ interface PostProps {
 export default function Post({ post }: PostProps) {
   const time = readTime(post.data.content);
   const router = useRouter();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    const anchor = document.getElementById('inject-comments-for-uterances');
+    script.setAttribute('src', 'https://utteranc.es/client.js');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', true);
+    script.setAttribute('repo', '[jukka10/ignite-desafio05]');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('theme', 'github-light');
+    anchor.appendChild(script);
+  }, []);
 
   if (router.isFallback) {
     return <div>Carregando...</div>;
@@ -69,7 +85,16 @@ export default function Post({ post }: PostProps) {
             <p>{time}</p>
           </div>
         </div>
-
+        <time className={styles.lastDate}>
+          * editado em{' '}
+          {format(new Date(post.last_publication_date), 'dd MMM yyyy', {
+            locale: ptBR,
+          })}{' '}
+          as,{' '}
+          {`${new Date(post.last_publication_date).getHours()}:${new Date(
+            post.last_publication_date
+          ).getMinutes()}`}
+        </time>
         <section className={styles.content}>
           {post.data.content.map(({ heading, body }, index) => (
             <div key={String(index)}>
@@ -82,6 +107,20 @@ export default function Post({ post }: PostProps) {
           ))}
         </section>
       </article>
+
+      <aside className={styles.otherPosts}>
+        <nav>
+          <div>
+            <span>Como utilizar Hooks</span>
+            <Link href="/">Post anterior</Link>
+          </div>
+          <div className={styles.boxNextPost}>
+            <span>Criando um app</span>
+            <Link href="/">Próximo post</Link>
+          </div>
+        </nav>
+      </aside>
+      <div id="inject-comments-for-uterances"></div>
     </>
   );
 }
@@ -113,6 +152,7 @@ export const getStaticProps: GetStaticProps = async context => {
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: Title,
       banner: { url: banner.url },
